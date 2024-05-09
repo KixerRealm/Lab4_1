e1fe82c22dda52abfd85b24ba9dd2b5c87211bf4
Kristijan Kostovski <kixer.wade2000@outlook.com>
2024-05-09 15:24:06 +0200

    Add project files.

---
+++ EShopAdminApplication/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShopAdminApplication.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public Guid ConcertId { get; set; }
+        public Concert? Concert { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+    }
+}
